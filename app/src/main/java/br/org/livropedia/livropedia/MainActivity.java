8463dc119f107de6b8f5d83a9cdd3c3c3e3cc800
RMFTI <rodrigo.medeiros.ti@gmail.com>
2016-11-03 18:51:19 -0200

    Adicionei um cometario

--- app/src/main/java/br/org/livropedia/livropedia/MainActivity.java
+++ app/src/main/java/br/org/livropedia/livropedia/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+//Meu comentario
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
