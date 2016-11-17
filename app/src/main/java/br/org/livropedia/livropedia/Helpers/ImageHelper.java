4b23d3786c335133bec9f0754d81dad3437fae49
Mayck Xavier <mayckxavier@gmail.com>
2016-11-17 20:07:54 -0200

    Implentando captura de fotos

---
+++ app/src/main/java/br/org/livropedia/livropedia/Helpers/ImageHelper.java
@@ -0,0 +1,35 @@
+package br.org.livropedia.livropedia.Helpers;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Project: qg2-agenteverde-android
+ * Package: br.com.qg2.agenteverde.helpers
+ * Author: Mayck Xavier <mayckxavier@lab01.com.br> http://lab01.com.br
+ * Date: 16/09/2016
+ */
+public class ImageHelper {
+
+
+    public static String encodeToBase64(Bitmap image) {
+        Bitmap immagex = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        immagex.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] b = baos.toByteArray();
+        String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT);
+
+        Log.e("LOOK", imageEncoded);
+        return imageEncoded;
+    }
+
+    public static Bitmap decodeBase64(String input) {
+        byte[] decodedByte = Base64.decode(input, 0);
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+    }
+
+}
